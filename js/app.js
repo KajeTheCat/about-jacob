d3fb3cc12abbaa8ac052d7b8c780db6bf07cf512
Jacob <jamsbury95@hotmail.com>
2021-09-29 20:03:29 -0700

    added greeting to js and a wall of text in the html

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let userName = '';
+
+function greetUser() {
+    userName = prompt('Hello friend! what is your name?');
+    console.log(userName)
+    alert('Hey ' + userName + ' it\'s great to meet you!')    
+}
+
+// function seeUser() {
+//     alert('Hey ' + userName + ' it\'s great to meet you!')
+// }
