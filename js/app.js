499d9296fdaeee5b450b649e3105969d0b740c66
Jacob <jamsbury95@hotmail.com>
2021-10-04 19:46:17 -0700

    added score count

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 //REMINDER: Ensure to invoke js functions in html with ()
 
+let score = 0;
+
 let userName = prompt('Hello friend! what is your name?');
     console.log(userName);
     alert('Hey ' + userName + ' it\'s great to meet you!');
@@ -10,6 +12,7 @@ let userAnswer1 = prompt('Do I own two animals?');
 
 if (userAnswer1.toLowerCase() === 'yes'  || userAnswer1.toLowerCase() === 'y') {
   alert('Yeah! A dog named Luca and a cat named Maxamillion!');
+  score++
 } else {
   alert('Actually, I do infact have two! A dog named Luca and a cat named Maxamillion!');
   //console.log(userAnswer1)
@@ -19,6 +22,7 @@ let userAnswer2 = prompt('Do I live in Washington?');
 
 if (userAnswer2.toLowerCase() === 'yes'  || userAnswer2.toLowerCase() === 'y') {
   alert('Yeah, I live an hour out from Seattle!');
+  score++
 } else {
   alert('Actually, yes infact I do!');
   //console.log(userAnswer2)
@@ -28,6 +32,7 @@ let userAnswer3 = prompt('Is my favorite color purple?');
 
 if (userAnswer3.toLowerCase() === 'no'  || userAnswer3.toLowerCase() === 'n') {
   alert('You are absolutely right, I prefer Green! lime to be exact.');
+  score++
 } else {
   alert('It is a very nice color, however my favorite is Lime green!');
   //console.log(userAnswer3)
@@ -37,6 +42,7 @@ let userAnswer4 = prompt('Do I enjoy tea?');
 
 if (userAnswer4.toLowerCase() === 'no'  || userAnswer4.toLowerCase() === 'n') {
   alert('Correct! it is a disgusting drink!');
+  score++
 } else {
   alert('Yuck, I cannot stand the taste!');
   //console.log(userAnswer4)
@@ -46,6 +52,7 @@ let userAnswer5 = prompt('Do I prefer computer games over console games?');
 
 if (userAnswer5.toLowerCase() === 'yes'  || userAnswer5.toLowerCase() === 'y') {
   alert('PC master race!');
+  score++
 } else {
   alert('Console noob, PC master race!');
   //console.log(userAnswer5)
@@ -56,6 +63,7 @@ for(let i = 4; i >= 1; i-- ) {
     let userAnswer6 = parseInt(prompt('Guess my favorite number'));
     if(userAnswer6 === correctAnswer) {
         alert('That\'s right!.');
+        score++
         break;
     } else if (userAnswer6 < correctAnswer) {
         alert(`Your guess is too low, try again. You have ${(i - 1)} attempts remaining.`);
@@ -76,6 +84,7 @@ for (let k = 6; k > guesses; k--) {
     if (userInput.toLowerCase() === favGames[j]) {
       alert('Bingo!');
       correct = true;
+      score++
     }
   }
   if (!correct) {
@@ -84,8 +93,8 @@ for (let k = 6; k > guesses; k--) {
   if (correct) break;
 }
 
-//let score = 0
+alert(`'possible answers could have been ${favGames}'`)
+
+alert(`'The total amount you\'ve gotten correct is ${score}'`)
 
 alert('Thank you for taking this short quiz ' + userName + '! Please enjoy reading about me down below.')
-
-//Wasn't given enough help to understand the full requirements of the course.
